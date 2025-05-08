38005b40cf1817728bc3829d792ce3d55158a8e3
mrSoftIce <mr.softice@gmail.com>
2025-05-09 00:01:23 +0400

    Homepage now shows list of models where flag is_on_main=True

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def index(request):
     template = 'homepage/index.html'
-    return render(request, template)
+
+    ice_cream_list = IceCream.objects.filter(is_on_main=True).all()
+    #.values('id', 'title', 'description')
+    context = {
+        'ice_cream_list': ice_cream_list
+    }
+    return render(request, template, context)
