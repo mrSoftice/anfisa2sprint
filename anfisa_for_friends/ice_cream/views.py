20fe581432eb1c0d23418a9e63e74d7d46ed5572
mrSoftIce <mr.softice@gmail.com>
2025-05-09 00:02:44 +0400

    Catalog connected to database. Shows list of all products, detailed page shows additional full information about selected model

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
+from ice_cream.models import IceCream
+
 
 def ice_cream_detail(request, pk):
     template = 'ice_cream/detail.html'
-    context = {}
+    ice_cream = IceCream.objects.get(id=pk)
+    context = {
+        'ice_cream': ice_cream
+    }
     return render(request, template, context)
 
 
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
-    context = {}
+    ice_cream_list = IceCream.objects.all()
+    context = {
+        'ice_cream_list': ice_cream_list
+    }
     return render(request, template, context)
