11b21ca19a3929ccc09aab4f4476c2a272c958df
mrSoftIce <mr.softice@gmail.com>
2025-05-13 00:43:17 +0400

    ice_cream list view

--- anfisa_for_friends/ice_cream/views.py
+++ anfisa_for_friends/ice_cream/views.py
@@ -26,7 +26,11 @@ def ice_cream_detail(request, pk):
 
 def ice_cream_list(request):
     template = 'ice_cream/list.html'
-    ice_cream_list = IceCream.objects.all()
+    ice_cream_list = IceCream.objects.select_related('category').filter(
+        is_published=True,
+        category__is_published=True
+    ).order_by('category')
+
     context = {
         'ice_cream_list': ice_cream_list
     }
