8f61de4b23aa98cdc71776187873aee32ffa4725
mrSoftIce <mr.softice@gmail.com>
2025-05-13 00:41:19 +0400

    homepage view

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from django.shortcuts import render
 
 from ice_cream.models import IceCream
@@ -8,14 +7,12 @@ def index(request):
     template = 'homepage/index.html'
 
     ice_cream_list = IceCream.objects.filter(
+        is_published=True,
+        is_on_main=True,
         category__is_published=True
-    ).filter(
-        Q(is_published=True) & (
-        Q(is_on_main=True) | Q(title__contains='пломбир')
-        )
     ).values(
-        'id', 'title', 'description', 'category__title'
-    )  # [1:4]
+        'id', 'title', 'price', 'description'
+    )
     context = {
         'ice_cream_list': ice_cream_list
     }
